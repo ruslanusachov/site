591e73a92e7c80f41b9761f32f16475a3a689a72
ArthurNameless <arthurrepin@gmail.com>
2019-04-23 21:18:54 +0300

    added JS folder

---
+++ JavaScript/main.js
@@ -0,0 +1,16 @@
+// var obj;
+
+// function sdfaa() {
+//   obj = 'Petya';
+// }
+
+
+// function afda () {
+//   if (obj == null || obj !== undefined){
+
+//   } else{
+
+//   }
+// }
+
+// ageVerify = prompt('А вам точно сколько лет?', '18') == 18;
