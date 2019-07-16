da606b3d91660d3710d88197c42776ae9dcfca50
ArthurNameless <arthurrepin@gmail.com>
2019-07-16 20:30:32 +0300

    fixed some issues

--- course-project/script.js
+++ course-project/script.js
@@ -3,24 +3,24 @@
 var head = document.getElementsByClassName('custome-heading')[0];
 var aboutUs = document.getElementById('about-us');
 head.onclick = function () {
-   aboutUs.classList.toggle('disappear');
- };
+  aboutUs.classList.toggle('disappear');
+};
 
 
- 
- 
+
+
 //  window.onscroll = function showBtn() {
 //    if(window.pageYOffset >= 400){
- 
+
 //      toTop.classList.remove('hidden');
-     
+
 //    } else if(window.pageYOffset < 400){
- 
+
 //      toTop.classList.add('hidden');
-     
+
 //    }
 //  };
- 
+
 //  window.onscroll = function() {
 
 //   if(window.pageYOffset >= 400){
@@ -31,26 +31,37 @@ head.onclick = function () {
 
 
 
-var whyUs = document.getElementById('advantages');
 
+function isScrolledIntoView(el) {
+  var rect = el.getBoundingClientRect();
+  var elemTop = rect.top;
+  var elemBottom = rect.bottom;
 
-window.onscroll = function showBlock () {
-    if (window.pageYOffset > 1370) {
+  // Only completely visible elements return true:
+  var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+  // Partially visible elements return true:
+  //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+  return isVisible;
+}
+
+window.onscroll = function showBlock() {
+  var whyUs = document.getElementById('advantages');
+
+  if (isScrolledIntoView(whyUs)) {
     whyUs.classList.remove('hidden');
 
-  }else if(window.pageYOffset < 1370) {
-    
+  } else {
     whyUs.classList.add('hidden');
-
-  } 
+  }
 };
 
 
 var toTop = document.getElementsByClassName('to-top')[0];
- toTop.onclick = function() {
-   window.scrollTo(0, 1000);
-   window.scrollTo({
-     top: 1000,
-     behavior: "smooth"
-   });
- };
+toTop.onclick = function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+
+};
